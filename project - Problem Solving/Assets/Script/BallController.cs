1d768401c3c1fdfe4bcf9013f87f5a35de87adb2
Ricky Limbaug <54813543+Animusphere@users.noreply.github.com>
2021-11-20 10:09:56 +0700

    1st commit
    finish

---
+++ project - Problem Solving/Assets/Script/BallController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+    // Besarnya gaya awal yang diberikan untuk mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+    // Titik asal lintasan bola saat ini
+    // private Vector2 trajectoryOrigin;
+    
+    private int starting = 0;
+    Vector2 keeper; //mengingat velonvity awal
+    
+    //ini akan dipindahkan ke gamemaneger
+    public ScoreManager skor;
+    public EnemyGenerator TraceON;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        // Mulai game
+        RestartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ResetBall()
+    {
+        // Reset posisi menjadi (0,0)
+        transform.position = Vector2.zero;
+
+        // Reset kecepatan menjadi (0,0)
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    void PushBall()
+    {
+        Vector2 newDirect;
+
+        //starting force
+        if (starting <= 0) { 
+            // float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+            // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
+            float randomDirection = Random.Range(0, 2);
+
+            // -1 = kiri 
+            if (randomDirection < 1.0f)
+                newDirect = new Vector2(-xInitialForce, yInitialForce);
+            else
+                newDirect = new Vector2(xInitialForce, yInitialForce);
+
+            //simpen nilai
+            keeper = newDirect; starting++;
+        }
+        //counter
+        else
+        {
+            float yDirection = Random.Range(-yInitialForce, yInitialForce);
+            float xDirection = Random.Range(-xInitialForce, xInitialForce);
+            Vector2 tmpDirect = new Vector2(xDirection, -yInitialForce); // buat arah baru
+            Vector2 newVel = keeper.magnitude * tmpDirect.normalized; //kecepatan awal dengan arah baru
+            newDirect = new Vector2(newVel.x,newVel.y);
+        }
+
+        rigidBody2D.AddForce(newDirect);
+    }   
+    void RestartGame()
+    {
+        // Kembalikan bola ke posisi semula
+        ResetBall();
+        // Setelah 2 detik, berikan gaya ke bola
+        Invoke("PushBall", 1);
+    }
+    // Ketika bola beranjak dari sebuah tumbukan, rekam titik tumbukan tersebut
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.tag == "enemy")
+        {
+            skor.AddScore(1);
+            Destroy(collision.gameObject);
+
+            StartCoroutine(delayKeneksi());
+        }
+    }
+
+    private IEnumerator delayKeneksi()
+    {
+        yield return new WaitForSeconds(3);
+
+        float getX = transform.position.x;
+        float getY = transform.position.y;
+        TraceON.ReTrance(getX, getY);
+    }
+}
---
+++ project - Problem Solving/Assets/Script/EnemyGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    //https://docs.unity3d.com/ScriptReference/Vector2.html
+    [Header("Area")]
+    public Vector2 mapSize;
+    public Vector2 offsetArea;
+
+    [Header("Enemy")]
+    public List<Sprite> enemyTypes = new List<Sprite>();
+    public GameObject enemyPrefab;
+    public int enemyCount;
+    public Vector2 offsetEnemy;
+
+    private List<GameObject> spawnedEnemy;
+    private List<Vector2> spawnPoint = new List<Vector2>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sizeEnemy = enemyPrefab.GetComponent<SpriteRenderer>().size;
+        //mengambil ukuran dari prefab
+        spawnedEnemy = new List<GameObject>(0);
+        spawnPoint.Add(new Vector2(0, 0)); //add to list
+        for (int i = 0; i < enemyCount; i++)
+        {
+            Tracing(i);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void EnemyOffset()
+    {
+        //total size = ukuran ditambah jarak dikali ukuran area - 1
+        //Vector2 totalSize = (sizeEnemy + offsetEnemy) * (size - Vector2.one);
+    }
+
+    private void Tracing(int i)
+    {
+        for (int j = 0; j <= i;)
+        {
+            float posX = Random.Range((-mapSize.x / 2) + offsetArea.x, (mapSize.x / 2) - offsetArea.x);
+            float posY = Random.Range((-mapSize.y / 2) + offsetArea.y, (mapSize.y / 2) - offsetArea.y);
+            
+            if (spawnPoint[j].x != posX && spawnPoint[j].y != posY)
+            {
+                TranceEnemy(posX, posY);
+                j++; break;
+            }
+        }
+    }
+
+    private void TranceEnemy(float posX, float posY)
+    {
+        GameObject newEnemy = Instantiate
+        (
+            enemyPrefab,
+            transform
+        );
+        newEnemy.transform.position = new Vector2(posX, posY);
+        spawnedEnemy.Add(newEnemy); //add to list
+        spawnPoint.Add(new Vector2(posX, posY)); //add to list
+    }
+
+    public void ReTrance(float oldX, float oldY)
+    {
+        while (true)
+        {
+            float posX = Random.Range((-mapSize.x / 2) + offsetArea.x, (mapSize.x / 2) - offsetArea.x);
+            float posY = Random.Range((-mapSize.y / 2) + offsetArea.y, (mapSize.y / 2) - offsetArea.y);
+            if (posX != oldX && posY != oldY)
+            {
+                TranceEnemy(posX, posY);
+                break;
+            }
+        }
+    }
+
+}
